9042c8a4f79369f28f49db989209dca85c1df32b
Sam Pita <samanthakpita@gmail.com>
2020-03-15 22:39:16 -0500

    created register model

---
+++ companytreeAPI/models/employee.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.db.models.expressions import F
+
+class Employee(models.Model):
+    '''Employee Model'''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    department = models.ForeignKey('Department', on_delete=models.DO_NOTHING, null=True)
+    supervisor = models.ForeignKey('Employee', on_delete=models.DO_NOTHING, null=True)
+    position = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    bio = models.CharField(max_length=300)
+    imageURL = models.CharField(max_length=300)
+    tasks = models.CharField(max_length=100)
+    phone = models.CharField(max_length=30)
+    isAdmin = models.BooleanField()
+
+    # def __str__(self):
+    #     return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        ordering = (F('id').asc(nulls_last=True),)
+        #Instances of F() act as a reference to a model field within a query. These references can then be used in query filters to compare the values of two different fields on the same model instance.
+        
+        #In this case, order by the date the user joined (by ascending) and null fields last.
